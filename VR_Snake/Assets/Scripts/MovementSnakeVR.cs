e14f77e615fa2beb3134c0ad71c6b34d3038a690
Tamer <tamer.ilguez@hotmail.de>
2018-06-26 14:42:47 +0200

    Add MovementSnake VR Basic +VR Controller

---
+++ VR_Snake/Assets/Scripts/MovementSnakeVR.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA;
+
+//Script for controlling the snake with the VR-Headset's position/rotation
+public class MovementSnakeVR : MonoBehaviour
+{
+   
+    float timeLeft =  VariableManager.instance.timeBetweenChangeDirection;
+
+    public void Update()
+    {
+        if (VariableManager.instance.useVRBasic)
+        {
+            //Debug.Log((InputTracking.GetLocalRotation(XRNode.Head)).ToString());
+            if (timeLeft <= 0)
+            {
+                changeDirection();
+                timeLeft = VariableManager.instance.timeBetweenChangeDirection;
+            }
+            timeLeft -= Time.deltaTime;
+        }
+    }
+
+    private void changeDirection()
+    {
+        if (InputTracking.GetLocalRotation(XRNode.Head).x > VariableManager.instance.sensitivityVertical)
+        {
+            MovementSnake.instance.rotateDown();
+        }
+        else if (InputTracking.GetLocalRotation(XRNode.Head).x < -VariableManager.instance.sensitivityVertical)
+        {
+            MovementSnake.instance.rotateUp();
+        }
+        if (InputTracking.GetLocalRotation(XRNode.Head).y > VariableManager.instance.sensitivityHorizontal)
+        {
+            MovementSnake.instance.rotateRight();
+        }
+        else if (InputTracking.GetLocalRotation(XRNode.Head).y < -VariableManager.instance.sensitivityHorizontal)
+        {   
+            MovementSnake.instance.rotateLeft();
+        }
+    }
+}
